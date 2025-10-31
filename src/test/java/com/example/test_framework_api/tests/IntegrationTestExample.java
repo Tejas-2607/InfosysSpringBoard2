42ffe3d51f38e955d1cf4365e06a209850062aab
Tejas-2607 <tejasj2607@gmail.com>
2025-11-01 01:20:03 +0530

    added Mock API's

--- src/test/java/com/example/test_framework_api/tests/IntegrationTestExample.java
+++ src/test/java/com/example/test_framework_api/tests/IntegrationTestExample.java
@@ -2,16 +2,27 @@ package com.example.test_framework_api.tests;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-// Integration tests (Pyramid middle: API interactions)
+import static org.hamcrest.Matchers.*; // <-- ADD THIS
+
+@ActiveProfiles("mock")
+@SpringBootTest
 public class IntegrationTestExample {
+
+    @Value("${external.api.base-url}")
+    private String baseUrl;
 
     @Test
     void apiTest() {
-        // REST-Assured example (Team 4 for API)
-        RestAssured.get("https://jsonplaceholder.typicode.com/todos/1")
-                .then()
-                .statusCode(200)
-                .body("title", org.hamcrest.Matchers.notNullValue()); // Assertion
+        RestAssured.given()
+            .baseUri(baseUrl)
+            .when()
+            .get("/todos/1")
+            .then()
+            .statusCode(200)
+            .body("title", is(notNullValue())); // OK
     }
 }
